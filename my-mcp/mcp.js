7d6fe4e35fc74b4055b3ccce753ec8a6b002fc20
simran7s <54254811+simran7s@users.noreply.github.com>
2025-09-17 00:26:47 -0400

    change zod version

--- my-mcp/mcp.js
+++ my-mcp/mcp.js
@@ -1,31 +1,31 @@
 import {
     McpServer,
-} from "@modelcontextprotocol/sdk/server/mcp.js";
-import {StdioServerTransport} from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
-
-const server = new McpServer({
+    ResourceTemplate,
+  } from "@modelcontextprotocol/sdk/server/mcp.js";
+  import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+  import { z } from "zod";
+  
+  // Create an MCP server
+  const server = new McpServer({
     name: "add-server",
-    version: "1.0.0"
-});
-
-server.registerTool(
+    version: "1.0.0",
+  });
+  
+  // Add an addition tool
+  server.registerTool(
     "add",
     {
-        title: "Addition Tool",
-        // This is the context that will be used to describe the tool is (Used by the LLM to determine when to use this tool). Shouldn't be too long. 1-3 sentences
-        description: "Add two numbers together",
-        // this helps the LLM understand the kind of data the tool required to function correctly.
-        inputSchema: {
-            a: z.number(),
-            b: z.number()
-        },
+      title: "Addition Tool",
+      description: "Add two numbers",
+      inputSchema: { a: z.number(), b: z.number() },
     },
-    async ({a, b}) => {
-        return {
-            content: [{type: "text", text: String(a + b)}]
+    async ({ a, b }) => {
+      return {
+          content: [{ type: "text", text: String(a + b) }],
+      }
     }
-});
-
-const transport = new StdioServerTransport();
-await server.connect(transport);
+  );
+  
+  // Start receiving messages on stdin and sending messages on stdout
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
